f0d92bd1822096026405429f006dd3c460beb022
neosamuri <neosamuri@gmail.com>
2026-07-14 07:45:21 +0000

    Fixed Dynamic counting via cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
+
   vite: {
     plugins: [tailwindcss()],
   },

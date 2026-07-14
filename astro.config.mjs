a9677b2f0b32023f735f174f7399a02a134d86d8
neosamuri <neosamuri@gmail.com>
2026-07-14 07:50:51 +0000

    change render back to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
-
   vite: {
     plugins: [tailwindcss()],
   },
